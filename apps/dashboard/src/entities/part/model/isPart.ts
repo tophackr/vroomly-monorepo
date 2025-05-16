f6ad1120e40168f9cc9b50b5a83342fe9d7057a7
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-05-16 15:37:08 +0300

    fix(dashboard): update list options

--- apps/dashboard/src/entities/part/model/isPart.ts
+++ apps/dashboard/src/entities/part/model/isPart.ts
@@ -1,6 +1,16 @@
-import { PartOption } from '@vroomly/prisma'
+import type { PartOption } from '@vroomly/prisma'
+
+// todo: this is a temporary solution that needs to be fixed.
+// enum does not return values, although the other one works correctly.
+const partOptions: PartOption[] = [
+    'oil',
+    'filter',
+    'brake_pads_and_discs',
+    'coolant',
+    'windshield_washer',
+    'spark_plugs'
+]
 
 export function isPart(value: string): value is PartOption {
-    const partOptions = Object.values(PartOption)
     return (partOptions as readonly string[]).includes(value)
 }
--- apps/dashboard/src/entities/repair/model/isRepair.ts
+++ apps/dashboard/src/entities/repair/model/isRepair.ts
@@ -1,6 +1,31 @@
-import { RepairOption } from '@vroomly/prisma'
+import type { RepairOption } from '@vroomly/prisma'
+
+// todo: this is a temporary solution that needs to be fixed.
+// enum does not return values, although the other one works correctly.
+const repairOptions: RepairOption[] = [
+    'engine_oil',
+    'transmission_oil',
+    'oil_filter',
+    'air_filter',
+    'cabin_filter',
+    'front_brake_pads',
+    'rear_brake_pads',
+    'brake_fluid',
+    'front_brake_discs',
+    'rear_brake_discs',
+    'air_conditioner_refill',
+    'spark_plugs',
+    'coolant',
+    'alignment',
+    'timing_belt',
+    'fuel_filter',
+    'transfer_case_oil',
+    'differential_oil',
+    'car_battery',
+    'power_steering_fluid',
+    'clutch'
+]
 
 export function isRepair(value: string): value is RepairOption {
-    const repairOptions = Object.values(RepairOption)
     return (repairOptions as readonly string[]).includes(value)
 }
