59f581c94b419b81070dd1142937f1d59b2c62c5
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-05-17 14:53:46 +0300

    refactor(dashboard): update body request

--- apps/dashboard/src/pages/stats/ui/parts-edit/hooks/useSaveParts.ts
+++ apps/dashboard/src/pages/stats/ui/parts-edit/hooks/useSaveParts.ts
@@ -1,9 +1,7 @@
 import { useCallback } from 'react'
 import { useCarContext } from '@/entities/car'
-import {
-    type RepairsProps,
-    useUpdateManyRepairMutation
-} from '@/entities/repair'
+import type { RepairsProps } from '@/entities/repair'
+import { useUpdateManyRepairMutation } from '@/entities/repair'
 import { useRouter } from '@/shared/i18n'
 import { useLogger } from '@/shared/model'
 import { statsRoute } from '@/shared/routes'
@@ -20,13 +18,26 @@ export function useSaveParts(): UseSavePartsReturns {
     const [updateMutation] = useUpdateManyRepairMutation()
 
     const saveCallback = useCallback(
-        (data: RepairsProps) =>
-            updateMutation({ carId: car.id, body: data.repairs }).then(
-                ({ error }) => {
-                    router.push(statsRoute.parts(car.id))
-                    if (error) logError('useSaveParts', error)
+        (data: RepairsProps) => {
+            const repairs = data.repairs.map(repair => {
+                const { id, option, mileage, days, isVisible, isDefault } =
+                    repair
+                return {
+                    id,
+                    mileage,
+                    days,
+                    isVisible,
+                    ...(isDefault ? {} : { option })
                 }
-            ),
+            })
+            return updateMutation({
+                carId: car.id,
+                body: { repairs }
+            }).then(({ error }) => {
+                router.push(statsRoute.parts(car.id))
+                if (error) logError('useSaveParts', error)
+            })
+        },
         [car.id, logError, router, updateMutation]
     )
 
