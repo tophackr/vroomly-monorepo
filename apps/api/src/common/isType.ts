266c08c7dea0061eed50e74aad7561d14b594e62
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-05-18 19:18:44 +0300

    refactor(api): add is type for popular interaction type

---
+++ apps/api/src/common/isType.ts
@@ -0,0 +1,22 @@
+import { InteractionType } from '@vroomly/prisma'
+
+const repairTypes: Set<InteractionType> = new Set([
+    InteractionType.maintenance,
+    InteractionType.repair
+])
+
+export function isFuelType(value: InteractionType): boolean {
+    return value === InteractionType.fuel
+}
+
+export function isRepairType(value: InteractionType): boolean {
+    return repairTypes.has(value)
+}
+
+export function isPartType(value: InteractionType): boolean {
+    return value === InteractionType.part
+}
+
+export function isWheelType(value: InteractionType): boolean {
+    return value === InteractionType.purchase_wheels
+}
