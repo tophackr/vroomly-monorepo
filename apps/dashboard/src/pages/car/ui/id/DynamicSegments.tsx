3abb90dbc454d68bbae29004b586303f51d02193
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-05-19 16:06:15 +0300

    refactor(dashboard): remove segments prop

--- apps/dashboard/src/pages/car/ui/id/DynamicSegments.tsx
+++ apps/dashboard/src/pages/car/ui/id/DynamicSegments.tsx
@@ -1,12 +1,11 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import { SegmentsSkeleton } from '@/features/segment'
 import { CarPreviewSkeleton } from '@/entities/car'
 
 export const DynamicSegments = dynamic(
     () => import('@/features/segment').then(i => i.Segments),
     {
-        loading: () => <CarPreviewSkeleton segments={<SegmentsSkeleton />} />
+        loading: () => <CarPreviewSkeleton />
     }
 )
