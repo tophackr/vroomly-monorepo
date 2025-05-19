73de249c984620fc7612236b65487629a5e284cb
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-05-19 18:21:19 +0300

    refactor(dashboard): enhance component with props and memoization

--- apps/dashboard/src/shared/ui/skeleton/SectionHeaderSkeleton.tsx
+++ apps/dashboard/src/shared/ui/skeleton/SectionHeaderSkeleton.tsx
@@ -1,5 +1,24 @@
+import type { HTMLAttributes } from 'react'
+import { memo } from 'react'
+import { cx } from '@/shared/lib/dom'
 import { TextSkeleton } from './TextSkeleton'
 
-export function SectionHeaderSkeleton() {
-    return <TextSkeleton className={'bg-subtitle ms-4 mt-8 mb-2'} />
+interface SectionHeaderSkeletonProps {
+    large?: boolean
 }
+
+export const SectionHeaderSkeleton = memo(function SectionHeaderSkeleton({
+    className,
+    large = false,
+    ...props
+}: HTMLAttributes<HTMLDivElement> & SectionHeaderSkeletonProps) {
+    return (
+        <TextSkeleton
+            className={cx(
+                large ? 'mb-4' : 'bg-subtitle ms-4 mt-8 mb-2',
+                className
+            )}
+            {...props}
+        />
+    )
+})
