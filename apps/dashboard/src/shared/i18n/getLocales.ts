ba87c8c31bfef779f1d0710bc1a58e167d0060de
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-05-21 19:07:59 +0300

    chore(dashboard): move eslint rules to first line

--- apps/dashboard/src/shared/i18n/getLocales.ts
+++ apps/dashboard/src/shared/i18n/getLocales.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return */
 import deepmerge from 'deepmerge'
 import { defaultLocale, locales } from './config'
 import type { Locale, TranslationConfig } from './types'
@@ -9,16 +10,12 @@ function isNotDefaultLocale(locale: Locale): boolean {
 export async function getLocales(
     locale: Locale = defaultLocale
 ): Promise<TranslationConfig> {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const [defaultMessages, userMessages] = await Promise.all([
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
         import(`./messages/${defaultLocale}.json`).then(m => m.default),
         isNotDefaultLocale(locale) &&
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
             import(`./messages/${locale}.json`).then(m => m.default)
     ])
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const mergedMessaged = userMessages
         ? // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
           deepmerge(defaultMessages, userMessages)
@@ -26,7 +23,6 @@ export async function getLocales(
 
     return {
         locale,
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         messages: mergedMessaged
     }
 }
