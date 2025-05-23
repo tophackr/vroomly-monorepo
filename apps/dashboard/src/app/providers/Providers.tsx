c33d60c6e9c2ba4c3406a4902030507c6ea2a702
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-05-23 13:49:37 +0300

    refactor(dashboard): remove memoized and i18n provider

--- apps/dashboard/src/app/providers/Providers.tsx
+++ apps/dashboard/src/app/providers/Providers.tsx
@@ -1,27 +1,19 @@
 import type { JSX, PropsWithChildren } from 'react'
-import { memo } from 'react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Toaster } from 'sonner'
 import { UserInitProvider } from '@/entities/user'
-import { I18nProvider } from '@/shared/i18n'
 import { MainContextProvider } from '@/shared/model'
 import { StoreProvider } from '../store/StoreProvider'
 import { TelegramProvider } from '../tma/TelegramProvider'
 
-export const Providers = memo(function Providers({
-    children
-}: PropsWithChildren): JSX.Element {
+export function Providers({ children }: PropsWithChildren): JSX.Element {
     return (
         <MainContextProvider>
             <StoreProvider>
                 <TelegramProvider>
-                    <UserInitProvider>
-                        <I18nProvider>{children}</I18nProvider>
-                        <Toaster />
-                        <SpeedInsights />
-                    </UserInitProvider>
+                    <UserInitProvider>{children}</UserInitProvider>
+                    <Toaster />
                 </TelegramProvider>
             </StoreProvider>
         </MainContextProvider>
     )
-})
+}
