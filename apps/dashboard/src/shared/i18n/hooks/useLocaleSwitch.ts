15ecc8214299b37973f8af1b63870b3bd3042adb
Alexandr Musikhin <spits.kin-0k@icloud.com>
2025-05-23 14:06:37 +0300

    refactor(dashboard): add transition for switch callback

--- apps/dashboard/src/shared/i18n/hooks/useLocaleSwitch.ts
+++ apps/dashboard/src/shared/i18n/hooks/useLocaleSwitch.ts
@@ -1,28 +1,30 @@
 'use client'
 
-import { useCallback } from 'react'
+import { useCallback, useTransition } from 'react'
 import { useParams } from 'next/navigation'
 import { usePathname, useRouter } from '../routing'
 import type { Locale } from '../types'
 
 export function useLocaleSwitch() {
+    const [isLoading, startTransition] = useTransition()
+    const params = useParams()
     const pathname = usePathname()
     const router = useRouter()
 
-    // With `pathnames`: Pass `params` as well
-    const params = useParams()
-
     const switchLocale = useCallback(
-        (locale: Locale) =>
-            router.replace(
-                // @ts-expect-error -- TypeScript will validate that only known `params`
-                // are used in combination with a given `pathname`. Since the two will
-                // always match for the current route, we can skip runtime checks.
-                { pathname, params },
-                { locale }
-            ),
+        (locale: Locale) => {
+            startTransition(() => {
+                router.replace(
+                    // @ts-expect-error -- TypeScript will validate that only known `params`
+                    // are used in combination with a given `pathname`. Since the two will
+                    // always match for the current route, we can skip runtime checks.
+                    { pathname, params },
+                    { locale }
+                )
+            })
+        },
         [params, pathname, router]
     )
 
-    return { switchLocale }
+    return { switchLocale, isLoading }
 }
